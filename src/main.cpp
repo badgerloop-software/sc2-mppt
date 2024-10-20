56bac96544e421e941c5ee7a683fcc1ad04ef30a
Aditya YV <adityayv802@gmail.com>
2024-10-20 10:55:12 -0500

    Added basic code

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,26 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+#define PWM_PIN PA_9
+#define PWM_FREQ 76923
+HardwareTimer *MyTim;
+uint32_t channel;
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  // Automatically retrieve TIM instance and channel associated to pin
+  // This is used to be compatible with all STM32 series automatically.
+  TIM_TypeDef *Instance = (TIM_TypeDef *)pinmap_peripheral(PWM_PIN, PinMap_PWM);
+  MyTim = new HardwareTimer(Instance);
+
+  uint32_t dutyCycle = 20; // in percentage, so 20 = 20%
+  channel = STM_PIN_CHANNEL(pinmap_function(PWM_PIN, PinMap_PWM));
+
+  MyTim->setPWM(channel, PWM_PIN, PWM_FREQ, dutyCycle); // set duty cycle
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  // test setting different dutyCycles
+  delay(5000);
+  MyTim->setPWM(channel, PWM_PIN, PWM_FREQ, 20);
+  delay(5000);
+  MyTim->setPWM(channel, PWM_PIN, PWM_FREQ, 50);
 }
