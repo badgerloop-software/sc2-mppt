aa8f887caad6de628befaa2067f3f4b09559eee7
Will Wentink <wwentink@wisc.edu>
2026-08-02 16:50:25 -0500

    Drop restating board_config header comments.
    Constants already live in board_config; the easy-to-change note added nothing.

--- include/board_config.h
+++ include/board_config.h
@@ -1,8 +1,6 @@
 #ifndef __BOARD_CONFIG_H__
 #define __BOARD_CONFIG_H__
 
-// all board constants so they are easy to change
-
 // ------------- TIMING -------------
 #define IO_UPDATE_PERIOD 100000                                   // us
 #define PID_UPDATE_PERIOD ((float)IO_UPDATE_PERIOD / 1000000.0f)  // PID interval is in seconds
