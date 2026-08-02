66d0158bb396098cddfb10952c31704ed2a79cb6
Will Wentink <wwentink@wisc.edu>
2026-08-02 18:32:14 -0500

    Bump embedded-pio for CAN_* ID rename.

--- src/can_mppt.cpp
+++ src/can_mppt.cpp
@@ -11,18 +11,18 @@ CanMppt::CanMppt(CAN_TypeDef* canPort, CAN_PINS pins, int frequency)
 void CanMppt::readHandler(CAN_message_t msg) {
     uint8_t* data = msg.buf;
     switch (msg.id) {
-        case SC2_CAN_MPPT_CAP_DISCHARGE_ID:
+        case CAN_MPPT_CAP_DISCHARGE:
             setCapDischarge(*data);
             break;
-        case SC2_CAN_MPPT_OV_RESET_ID:
+        case CAN_MPPT_OV_RESET:
             clearOVFaultReset(*data);
             break;
-        case SC2_CAN_BMS_PACK_ID:
+        case CAN_BMS_PACK:
             // bytes 0-1: current * 0.1 A, byte 4: SOC * 0.5 %
             packCurrent = ((data[0] << 8) + data[1]) * 0.1f;
             packSOC = (float)(data[4]) / 2.0f;
             break;
-        case SC2_CAN_BMS_CHARGE_LIMIT_ID:
+        case CAN_BMS_CHARGE_LIMIT:
             packChargeCurrentLimit = (float)(*(uint16_t*)data) * CONST_CURR_SAFETY_MULT;
             break;
         default:
@@ -31,25 +31,25 @@ void CanMppt::readHandler(CAN_message_t msg) {
 }
 
 void CanMppt::sendMpptData() {
-    sendMessage(SC2_CAN_MPPT_BOOST_ID, (void*)&boostEnabled, sizeof(boostEnabled));
-    sendMessage(SC2_CAN_MPPT_MODE_ID, (void*)&chargeMode, sizeof(ChargeMode));
+    sendMessage(CAN_MPPT_BOOST, (void*)&boostEnabled, sizeof(boostEnabled));
+    sendMessage(CAN_MPPT_MODE, (void*)&chargeMode, sizeof(ChargeMode));
 
     for (int i = 0; i < NUM_ARRAYS; i++) {
-        sendMessage(SC2_CAN_MPPT_STRING_ID(i, SC2_CAN_MPPT_FIELD_V), (void*)&(arrayData[i].voltage),
+        sendMessage(CAN_MPPT_STRING(i, CAN_MPPT_FIELD_V), (void*)&(arrayData[i].voltage),
                     sizeof(float));
-        sendMessage(SC2_CAN_MPPT_STRING_ID(i, SC2_CAN_MPPT_FIELD_I), (void*)&(arrayData[i].current),
+        sendMessage(CAN_MPPT_STRING(i, CAN_MPPT_FIELD_I), (void*)&(arrayData[i].current),
                     sizeof(float));
-        sendMessage(SC2_CAN_MPPT_STRING_ID(i, SC2_CAN_MPPT_FIELD_TEMP), (void*)&(arrayData[i].temp),
+        sendMessage(CAN_MPPT_STRING(i, CAN_MPPT_FIELD_TEMP), (void*)&(arrayData[i].temp),
                     sizeof(float));
-        sendMessage(SC2_CAN_MPPT_STRING_ID(i, SC2_CAN_MPPT_FIELD_DUTY),
+        sendMessage(CAN_MPPT_STRING(i, CAN_MPPT_FIELD_DUTY),
                     (void*)&(arrayData[i].dutyCycle), sizeof(float));
         if (chargeMode == ChargeMode::MPPT) {
-            sendMessage(SC2_CAN_MPPT_STRING_ID(i, SC2_CAN_MPPT_FIELD_TARGET),
+            sendMessage(CAN_MPPT_STRING(i, CAN_MPPT_FIELD_TARGET),
                         (void*)&(targetVoltage[i]), sizeof(float));
         } else {
             // no target in const current mode
             float none = -1.0f;
-            sendMessage(SC2_CAN_MPPT_STRING_ID(i, SC2_CAN_MPPT_FIELD_TARGET), (void*)&none,
+            sendMessage(CAN_MPPT_STRING(i, CAN_MPPT_FIELD_TARGET), (void*)&none,
                         sizeof(float));
         }
     }
